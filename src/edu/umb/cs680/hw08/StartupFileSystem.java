e9e8fd4625233eefa4d42301c80b80be65e4d307
Cole Maxwell <cole.h.maxwell@gmail.com>
2020-12-18 17:47:09 -0500

    minor update

--- src/edu/umb/cs680/hw08/StartupFileSystem.java
+++ src/edu/umb/cs680/hw08/StartupFileSystem.java
@@ -13,7 +13,7 @@ public class StartupFileSystem {
         Directory code = new Directory(home, "code", 0, LocalDateTime.now(), null);
         
         File a = new File(null, "a.txt", 8, LocalDateTime.now(), null);
-        File b = new File(null, "b.txt", 16, LocalDateTime.now(), null);
+        File b = new File(null, "b.txt", 512, LocalDateTime.now(), null);
         File c = new File(null, "c.txt", 32, LocalDateTime.now(), null);
         File d = new File(null, "d.txt", 64, LocalDateTime.now(), null);
         File e = new File(null, "e.txt", 128, LocalDateTime.now(), null);
@@ -39,7 +39,9 @@ public class StartupFileSystem {
             System.out.println("Link Name: " + fsElement.getName());
             System.out.println("Link Size: " + fsElement.getSize());
             System.out.println("Link Creation time: " + fsElement.getCreationTime());
+            System.out.println("Is link: " + fsElement.isDirectory());
             System.out.println("Is directory: " + fsElement.isDirectory());
+            System.out.println("Is file: " + fsElement.isDirectory());
             System.out.println("Link Parent name: " + fsElement.getParent().getName());
             System.out.println("Link Target name: " + fsElement.getTarget().getName()); // link
             System.out.println("Link Target size: " + fsElement.getTarget().getSize()); // 0
@@ -50,13 +52,13 @@ public class StartupFileSystem {
             System.out.println("Link Name: " + fsElement.getName());
             System.out.println("Link Size: " + fsElement.getSize());
             System.out.println("Link Creation time: " + fsElement.getCreationTime());
-            System.out.println("Is directory: " + fsElement.isDirectory());
+            System.out.println("Is link: " + fsElement.isDirectory());
             System.out.println("Link Parent name: " + fsElement.getParent().getName());
             System.out.println("Link Target name: " + fsElement.getTarget().getName()); // link
             System.out.println("Link Target size: " + fsElement.getTarget().getSize()); // 0
             System.out.println("Link Target Creation Time: " + fsElement.getTarget().getCreationTime());
             System.out.println();
-        }       
+        }
 	}
 }
 
